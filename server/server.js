e6cb1aae443046323ebcd5a6f841809a56726772
Vo <vo@Vos-iMac.local>
2018-08-16 07:48:26 +1000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -22,6 +23,8 @@ app.post('/todos', (req, res) => {
 
 });
 
+
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) =>{
     res.send({todos});
@@ -29,6 +32,24 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res)=>{
+    const id = req.params.id;
+    if (!ObjectID.isValid(id)){
+      return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }).catch((e) => {
+      res.status(400).send();
+    })
+});
+
+
 
 
 app.listen(3000, () => {
